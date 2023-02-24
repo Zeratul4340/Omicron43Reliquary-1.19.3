a1c56e92ed6d61d934706dd9fca2e023587c5976
Zeratul4340 <eduardoatroom37@gmail.com>
2023-02-24 01:26:39 -0800

    test1

---
+++ src/main/java/net/omicron43/reliquarymod/Omicron43Reliquary.java
@@ -0,0 +1,20 @@
+package omicron43.reliquarymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Omicron43Reliquary implements ModInitializer {
+	public static final String MOD_ID = "omicron43s-reliquary";
+	public static final Logger LOGGER = LoggerFactory.getLogger("omicron43reliquary");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/omicron43/reliquarymod/Omicron43ReliquaryClient.java
@@ -0,0 +1,10 @@
+package net.omicron43.reliquarymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Omicron43ReliquaryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
