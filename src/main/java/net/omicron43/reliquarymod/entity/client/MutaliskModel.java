47dca7c82d77ee26a1fbaf9883418c0d6ab7aed8
Zeratul4340 <eduardoatroom37@gmail.com>
2023-04-10 18:36:48 -0700

    MutaliskModel.java progress

--- src/main/java/net/omicron43/reliquarymod/entity/client/MutaliskModel.java
+++ src/main/java/net/omicron43/reliquarymod/entity/client/MutaliskModel.java
@@ -1,20 +1,26 @@
 package net.omicron43.reliquarymod.entity.client;
 
-import net.minecraft.util.Identifier;
 import net.omicron43.reliquarymod.Omicron43Reliquary;
 import net.omicron43.reliquarymod.entity.custom.MutaliskEntity;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import software.bernie.geckolib.constant.DataTickets;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
+import software.bernie.geckolib.model.data.EntityModelData;
+
 
 public class MutaliskModel extends GeoModel<MutaliskEntity> {
     
 
         @Override
-        public Identifier getModelResource(MutaliskEntity object) {
+        public Identifier getModelResource(MutaliskEntity animatable) {
             return new Identifier(Omicron43Reliquary.MOD_ID, "geo/mutalisk.geo.json");
         }
 
         @Override
-        public Identifier getTextureResource(MutaliskEntity object) {
+        public Identifier getTextureResource(MutaliskEntity animatable) {
             return new Identifier(Omicron43Reliquary.MOD_ID, "textures/entity/mutalisk/omicron43mutalisktexture.png");
         }
 
@@ -23,5 +29,14 @@ public class MutaliskModel extends GeoModel<MutaliskEntity> {
             return  new Identifier(Omicron43Reliquary.MOD_ID, "animations/mutalisk.animation.json");
         }
 
+        @Override
+        public void setCustomAnimations(MutaliskEntity animatable, long instanceId, AnimationState<MutaliskEntity> animationState) {
+            CoreGeoBone head = getAnimationProcessor().getBone("head");
 
+            if (head != null) {
+                EntityModelData entityData = animationState.getData(DataTickets.ENTITY_MODEL_DATA);
+                head.setRotX(entityData.headPitch() * MathHelper.RADIANS_PER_DEGREE);
+                head.setRotY(entityData.netHeadYaw() * MathHelper.RADIANS_PER_DEGREE);
+            }
+        }
     }
